3590f4286c0a2149092162a01e5547773cf9d978
Jmart10 <94086404+Jmart10@users.noreply.github.com>
2025-03-09 12:59:39 -0500

    Update noticia.js

--- noticia.js
+++ noticia.js
@@ -1,4 +1,4 @@
-let boton = document.getElementsByClassName('boton')[0];
+let boton = document.getElementsByClassName('abrir')[0];
 
 
 
@@ -9,5 +9,28 @@ boton.addEventListener('click', (e) => {
         title: "Estamos embarazados",
         icon: "success",
         draggable: true
-      });
+    });
+      for (let i = 0; i < 30; i++) { // Generar 10 emojis
+        let emoji = document.createElement('span');
+        emoji.classList.add('emoji');
+
+        let emojisFelicidad = ['😊', '🎉', '🥳','🎊'];
+        emoji.textContent = emojisFelicidad[Math.floor(Math.random() * emojisFelicidad.length)];
+
+        document.body.appendChild(emoji);
+
+        // Posicionar el emoji cerca del botón
+        let x = e.clientX + (Math.random() * 400 - 50);
+        let y = e.clientY + (Math.random() * 400 - 50);
+        emoji.style.left = `${x}px`;
+        emoji.style.top = `${y}px`;
+
+        // Eliminar el emoji después de la animación
+        setTimeout(() => {
+            emoji.remove();
+            
+        }, 2000);
+
+        
+    }
 });
